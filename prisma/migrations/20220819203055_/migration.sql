a7950e241f182ab3ca7b6be96ee81772a0e14ba0
Andrei Rivera <andreirive2003@gmail.com>
2022-08-19 15:34:16 -0600

    Table and migration to Postgresql

---
+++ prisma/migrations/20220819203055_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
